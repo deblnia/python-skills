a849b1b9b8dd757124a0ba14700e2ab7f206288c
deblina <deblina@uchicago.edu>
2024-11-04 15:07:09 -0500

    function

--- mastering-python/part1/pcost.py
+++ mastering-python/part1/pcost.py
@@ -1,14 +1,17 @@
-# opens this file, reads all lines, and calculates how much it cost to purchase
+#opens this file, reads all lines, and calculates how much it cost to purchase
 # all of the shares in the portfolio. To do this, compute the sum of the second
 # column multiplied by the third column.
 
-total_cost = 0.0 
+def portfolio_cost(file):
+    total_cost = 0.0
 
-with open('./data/portfolio.dat', 'r') as f: 
-    for line in f: 
-        row = line.split()
-        total_cost += (float(row[1]) + float(row[2]))
-    print(total_cost) 
+    with open(file, 'r') as f: 
+        for line in f: 
+            row = line.split()
+            total_cost += (float(row[1]) + float(row[2]))
+    return total_cost 
+
+print(portfolio_cost('Data/portfolio.dat'))
 
 
 

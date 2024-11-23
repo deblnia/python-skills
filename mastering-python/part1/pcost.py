b017870e98927001f4904b7917fcd2859da84358
deblina <deblina@uchicago.edu>
2024-11-22 21:34:00 -0500

    1.5

--- mastering-python/part1/pcost.py
+++ mastering-python/part1/pcost.py
@@ -6,12 +6,12 @@ def portfolio_cost(file):
     total_cost = 0.0
 
     with open(file, 'r') as f: 
-        for line in f: 
-            row = line.split()
+        for line in f:
+            row = lin e.split()
             total_cost += (float(row[1]) + float(row[2]))
     return total_cost 
 
 print(portfolio_cost('Data/portfolio.dat'))
 
-
+# I like the suggestion in the exercise to run this interactively python3 -i file 
 

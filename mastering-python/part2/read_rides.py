2dc8d174ee2ed0a7e944f88b23ba26ec4a21014f
deblina <deblina@uchicago.edu>
2024-12-25 14:48:00 -0500

    starting 2.1

--- mastering-python/part2/read_rides.py
+++ mastering-python/part2/read_rides.py
@@ -1,1 +1,56 @@
+import csv
+import tracemalloc
 
+# what's the source of the overhead when reading in as strings? 
+
+
+def read_rides_as_tuples(filename):
+    '''
+    Read the bus ride data as a list of tuples
+    '''
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)     # Skip headers
+        for row in rows:
+            route = row[0]
+            date = row[1]
+            daytype = row[2]
+            rides = int(row[3])
+            record = (route, date, daytype, rides)
+            records.append(record)
+    return records
+
+def read_rides_as_dicts(filename):
+    '''
+    Read the bus ride data as a list of dictionaries 
+    '''
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)  # Skip headers
+        d = {}
+        for row in rows:
+            route = row[0]
+            date = row[1]
+            daytype = row[2]
+            rides = int(row[3])
+            record = {
+                'route': route, 
+                'date': date, 
+                'daytype': daytype, 
+                'rides' : rides
+                }
+            records.append(record)
+    return records
+
+# A class 
+
+# A named tuple 
+
+# A class with __slots__
+
+if __name__ == '__main__':
+    tracemalloc.start()
+    rows = read_rides_as_tuples('data/ctabus.csv')
+    print('Memory Use: Current %d, Peak %d' % tracemalloc.get_traced_memory())

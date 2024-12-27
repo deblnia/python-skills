a169d72861da1963eda0821931c3fef059959797
deblina <deblina@uchicago.edu>
2024-12-27 13:55:41 -0500

    2_3

--- mastering-python/2_3.py
+++ mastering-python/2_3.py
@@ -39,13 +39,54 @@ for i, v in enumerate(rows):
 # can still unpack with enumerate 
 for i, (name, shares, price) in enumerate(rows): 
     print(f"enumerate with unpacking example: \n  {i}, {name}, {share}, {price} ")
-    break # this is also an important part of iteration IMO 
+    break 
+    # this is also an important part of iteration IMO 
     # break will break out of the current loop 
     # continue will break out of the current iteration of the loop
 
 # zip can be used to pair data 
-for col, val in zip(headers, row[0]): 
+print("pairing data with zip: \n")
+for col, val in zip(headers, rows[0]): 
     print(col, val)
+    break
+
+# or make a dictionary 
+print("making dictionary with zip: \n")
+zd = dict(zip(headers, row))
+print(zd.items())
+
+# or sequence of dictionaries 
+for row in rows: 
+    record = dict(zip(headers, row))
+    print(f"sequence of dictionaries: \n {record}")
+
+# generators 
+nums = [1,2,3,4,5]
+squares = (x*x for x in nums)
+squares 
+
+for n in squares: 
+    print(n)
+
+print("Generators can only be used once.")
+for n in squares:
+    print(n)
+
+# next and yield are important for generators 
+squares = (x*x for x in nums)
+print(next(squares)) # gives results one at a time 
+
+def squares(nums): 
+    for x in nums: 
+        yield x*x # feeds nicely to the for 
+
+for n in squares(nums):
+    print(n)
+
+
+
+
+
 
 
 
